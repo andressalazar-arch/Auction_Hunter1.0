3fbc9a09d2f5f52b1165ab0cf4b7c301b512937e
andressalazar-arch <andres.salazar@blueflameestates.com>
2026-03-05 22:40:24 +0000

    Update auctionhouse.ts

--- auctionhouse.ts
+++ auctionhouse.ts
@@ -1,9 +1,7 @@
-import * as cheerio from "cheerio";
-import { fetchHtml, sleep } from "../../utils/fetch.js";
-import { dedupeKey } from "../../utils/hash.js";
-import { extractBedrooms, extractGuidePrice, extractPostcode } from "../../utils/parse.js";
-import type { ScrapedLot } from "../../utils/types.js";
-
+import { fetchHtml, sleep } from "./fetch.js";
+import { dedupeKey } from "./hash.js";
+import { extractBedrooms, extractGuidePrice, extractPostcode } from "./parse.js";
+import type { ScrapedLot } from "./types.js";
 const SOURCE = "Auction House";
 const LIST_URL = "https://www.auctionhouse.co.uk/unsold";
 
